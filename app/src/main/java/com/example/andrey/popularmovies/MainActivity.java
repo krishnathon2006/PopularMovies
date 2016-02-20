092ac3457002853162bfc7914e60017a5b86c8c2
Andrey Andreev <andrejev2006@gmail.com>
2016-02-20 01:25:03 -0800

    Add two fragments

--- app/src/main/java/com/example/andrey/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/andrey/popularmovies/MainActivity.java
@@ -1,92 +1,13 @@
 package com.example.andrey.popularmovies;
 
-import android.app.Activity;
-import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.GridView;
+import android.support.v4.app.FragmentActivity;
 
-import com.example.andrey.popularmovies.adapter.GridViewAdapter;
-import com.example.andrey.popularmovies.dao.MovieDb;
-import com.example.andrey.popularmovies.model.Movie;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class MainActivity extends Activity {
-    private static String LOG_TAG = MainActivity.class.getSimpleName();
-    private GridViewAdapter gridAdapter;
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        GridView gridView = (GridView) findViewById(R.id.movies_grid_view);
-        gridAdapter = new GridViewAdapter(this, R.layout.image_item_view, new ArrayList<Movie>());
-        gridView.setAdapter(gridAdapter);
-
-        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Movie item =  (Movie)parent.getItemAtPosition(position);
-
-                Intent intent = new Intent(MainActivity.this, MovieDetailPageActivity.class);
-                intent.putExtra("movie", item);
-
-                startActivity(intent);
-            }
-        });
-
-        MovieListRequestOperation movieListRequestOperation = new MovieListRequestOperation();
-        movieListRequestOperation.execute();
-    }
-
-    private class MovieListRequestOperation extends AsyncTask<Void, Void, JSONArray> {
-        @Override
-        protected JSONArray doInBackground(Void... params) {
-            int pageToRequest = 1;
-            JSONArray result = MovieDb.getNowPlayingMovies(pageToRequest);
-            Log.v(LOG_TAG, result.toString());
-            return result;
-        }
-
-        @Override
-        protected void onPostExecute(JSONArray jsonArray) {
-            if (jsonArray == null) {
-                return;
-            }
-
-            // Convert JSONArray with movies to movie list
-            List<Movie> movies = new ArrayList<>(20);
-            // I intentionally do not use the GSON library here: https://github.com/google/gson
-            // GSON ~300Kb library
-            for (int i = 0; i < jsonArray.length(); i++) {
-                try {
-                    JSONObject movieObject = jsonArray.getJSONObject(i);
-                    int id = movieObject.getInt("id");
-                    String posterPath = movieObject.getString("poster_path");
-                    String overView = movieObject.getString("overview");
-                    String title = movieObject.getString("title");
-                    float voteAverage = (float)movieObject.getDouble("vote_average");
-                    Movie movie = new Movie(id, overView, posterPath, title, voteAverage);
-                    movies.add(movie);
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            Log.v(LOG_TAG, movies.toString());
-            gridAdapter.clear();
-            gridAdapter.addAll(movies);
-            gridAdapter.notifyDataSetChanged();
-        }
     }
 }
--- app/src/main/java/com/example/andrey/popularmovies/MovieDetailPageActivity.java
+++ app/src/main/java/com/example/andrey/popularmovies/MovieDetailPageActivity.java
@@ -1,38 +1,85 @@
 package com.example.andrey.popularmovies;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.TextView;
 
 import com.example.andrey.popularmovies.model.Movie;
+import com.example.andrey.popularmovies.util.Constants;
 import com.squareup.picasso.Picasso;
 
-public class MovieDetailPageActivity extends AppCompatActivity {
+public class MovieDetailPageActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_detail_page);
 
-        Movie movie = (Movie)getIntent().getSerializableExtra("movie");
-
-        TextView movieTitleComp = (TextView) findViewById(R.id.dp_movieTitle);
-        movieTitleComp.setText(movie.getTitle());
-
-        TextView movieDescriptionComp = (TextView) findViewById(R.id.dp_movieDescription);
-        movieDescriptionComp.setText(movie.getOverview());
-
-        RatingBar movieRatingComp = (RatingBar) findViewById(R.id.dp_movieRating);
-        movieRatingComp.setRating(movie.getVoteAverage());
-
-        ImageView movieImage = (ImageView)findViewById(R.id.dp_movieImage);
-        Picasso
-                .with(this)
-                .load("http://image.tmdb.org/t/p/original/" + movie.getPosterPath())
-                .fit()
-                .into(movieImage);
+        MovieDetailPageFragment movieDetailPageFragment = (MovieDetailPageFragment) getSupportFragmentManager().findFragmentById(R.id.dp_movieContainer);
+        Movie movie = (Movie) getIntent().getSerializableExtra("movie");
+        movieDetailPageFragment.updateContent(movie);
     }
 
+
+    public static class MovieDetailPageFragment extends Fragment {
+        private Movie movie;
+
+        public MovieDetailPageFragment() {
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            if (savedInstanceState != null) {
+                movie = (Movie) savedInstanceState.getSerializable("movie");
+            }
+            View rootView = inflater.inflate(R.layout.fragment_movie_detail_page, container, false);
+//            updateValuesInComponents(movie);
+
+            return rootView;
+        }
+
+        @Override
+        public void onStart() {
+            super.onStart();
+            updateValuesInComponents(movie);
+        }
+
+        public void updateContent(Movie movie) {
+            this.movie = movie;
+            updateValuesInComponents(movie);
+        }
+
+        @Override
+        public void onSaveInstanceState(Bundle saveInstantState) {
+            saveInstantState.putSerializable("movie", movie);
+        }
+
+        private void updateValuesInComponents(Movie movie) {
+            if (movie != null && getView() != null) {
+                TextView movieTitleComp = (TextView) getActivity().findViewById(R.id.dp_movieTitle);
+                movieTitleComp.setText(movie.getTitle());
+
+                TextView movieDescriptionComp = (TextView) getActivity().findViewById(R.id.dp_movieDescription);
+                movieDescriptionComp.setText(movie.getOverview());
+
+                RatingBar movieRatingComp = (RatingBar) getActivity().findViewById(R.id.dp_movieRating);
+                movieRatingComp.setRating(movie.getVoteAverage());
+
+                ImageView movieImage = (ImageView) getActivity().findViewById(R.id.dp_movieImage);
+                Picasso
+                        .with(getActivity())
+                        .load(Constants.urlToMoviePoster + movie.getPosterPath())
+                        .fit()
+                        .into(movieImage);
+            }
+        }
+    }
 }
